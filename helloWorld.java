19be75eae7392aaae7afdee98b19b37aa68fcdc5
Yash Alapuria <67858039+Yashraj2502@users.noreply.github.com>
2021-10-21 21:24:57 +0530

    Create helloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,9 @@
+//Hello World program in java
+//Here, name of my file was "helloWorld.java"
+
+public class helloWorld {
+    public static void main(String[] args){
+        System.out.print("\033[H\033[2J"); // - This line clears the screen.
+        System.out.println("Hello World");
+    }
+}
